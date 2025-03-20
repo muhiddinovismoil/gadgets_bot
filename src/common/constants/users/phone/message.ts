142e743c89697395911f67562e120a039db20ae0
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-03-20 17:46:45 +0500

    messages and keyboard folder created with files  for pc and phones

---
+++ src/common/constants/users/phone/message.ts
@@ -0,0 +1,20 @@
+export const askModelPhoneMsg = {
+  uz: `Telefon modelini kiriting`,
+  en: `Enter the phone model`,
+  ru: `Введите модель телефона`,
+};
+export const askStoragePhoneMsg = {
+  uz: `Telefon xotirasini kiriting misol uchun: 4/64`,
+  en: `Enter phone memory, for example: 4/64`,
+  ru: `Введите память телефона, например: 4/64`,
+};
+export const askPricePhoneMsg = {
+  uz: `Telefon narxini kiriting`,
+  en: `Enter the price of the phone`,
+  ru: `Введите стоимость телефона`,
+};
+export const askUsernamePhoneSeller = {
+  uz: 'Usernameingizni kiriting',
+  ru: 'Введите свое имя пользователя',
+  en: 'Enter your username',
+};
